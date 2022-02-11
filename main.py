3faca5bab733c066f0caa201cc98d846e0ec7659
finnjefferis <91462667+finnjefferis@users.noreply.github.com>
2022-02-11 12:24:59 +0000

    Added ASCII art and in-terminal graphs

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-#SUPER QUICKSORT 
-
-#select pivot (random or median element)
-#move pivot to the end of the array
-#move the element at the start of the array towards the end until it reaches a value greater or equal to the PIVOT
-#move the end element towards the start of the array until it crosses the start bound or finds a value less than the pivot 
-
-
+#SUPER QUICKSORT
+                                                                                            
 import numpy as np
+import termplotlib as tpl
 
 
 #initialise array of randomised integers
@@ -19,7 +14,33 @@ end = randnums[11]
 #declare pivot
 pivot = start
 
-
+#print values to console
+print("""███████╗██╗   ██╗██████╗ ███████╗██████╗      ██████╗ ██╗   ██╗██╗ ██████╗██╗  ██╗███████╗ ██████╗ ██████╗ ████████╗
+██╔════╝██║   ██║██╔══██╗██╔════╝██╔══██╗    ██╔═══██╗██║   ██║██║██╔════╝██║ ██╔╝██╔════╝██╔═══██╗██╔══██╗╚══██╔══╝
+███████╗██║   ██║██████╔╝█████╗  ██████╔╝    ██║   ██║██║   ██║██║██║     █████╔╝ ███████╗██║   ██║██████╔╝   ██║   
+╚════██║██║   ██║██╔═══╝ ██╔══╝  ██╔══██╗    ██║▄▄ ██║██║   ██║██║██║     ██╔═██╗ ╚════██║██║   ██║██╔══██╗   ██║   
+███████║╚██████╔╝██║     ███████╗██║  ██║    ╚██████╔╝╚██████╔╝██║╚██████╗██║  ██╗███████║╚██████╔╝██║  ██║   ██║   
+╚══════╝ ╚═════╝ ╚═╝     ╚══════╝╚═╝  ╚═╝     ╚══▀▀═╝  ╚═════╝ ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   
+                                                                                                                    """)
 print (randnums)
-print(end)
 print("Pivot is " +str(pivot))
+rng = np.random.default_rng(123)
+sample = rng.standard_normal(size=1000)
+counts, bin_edges = np.histogram(sample)
+
+fig = tpl.figure()
+fig.hist(counts, bin_edges, orientation="horizontal", force_ascii=False)
+fig.show()
+
+
+#move the element at the start of the array towards the end until it reaches a value greater or equal to the PIVOT
+while start < end:
+    while start < len(randnums) and randnums[start] <= pivot:
+            start += 1
+
+#move the end element towards the start of the array until it crosses the start bound or finds a value less than the pivot 
+    while randnums[end] > pivot:
+        end -= 1
+         
+             
+
